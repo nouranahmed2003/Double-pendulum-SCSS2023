e1f06647ff6cc1935de178d16fb211d8c166d5bd
Adrian-Tao Lai <laiadriantao@zohomail.eu>
2023-04-23 10:40:16 +0300

    Last Week Ideas, Moving to Computer rn

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
-let fixedBar = 230;
+/*
+  Default starting values
+*/
+let fixedBar = 120;
 let attachedBar = 120;
-let massFixedObject = 1000;
-let massAttachedObject = 1000;
+let massFixedObject = 10;
+let massAttachedObject = 1;
 
+/* 
+  a1 = fixedBarAngle
+  a2 = attachedBarAngle
+*/
 let a1 = 0; 
-let a2 = 0; 
+let a2 = 0;
+let a1_change = 90;
+let a2_change = 90; 
 
 let angularVelocityFixedObject = 0;
-
 let a1_v = angularVelocityFixedObject;
 
 let angularVelocityAttachedObject = 0;
-
 let a2_v = angularVelocityAttachedObject;
 
 let gravitationalAcceleration = 0.981;
@@ -19,9 +26,18 @@ let gravitationalAcceleration = 0.981;
 let px2 = 0; 
 let py2 = 0;
 
+/*
+  cx, cy - coordinates of origin of the the system of fixed bar
+*/
 let cx, cy;
 
 let buffer;
+let pendulumAnimation = true;
+
+/*
+  Overriding values if input is found
+*/
+let entry1;
 
 
 function setup() {
@@ -35,40 +51,29 @@ function setup() {
 
   This is because the motion is determined by simple sine and cosine functions.
   For large angles, the pendulum is non-linear and the phase graph becomes much more complex.
-
-  a1 = fixedBarAngle
-  a2 = attachedBarAngle
   */
-
- 
-
-
   
-  Change = PI / 180;
-  a1 = 90*Change; 
-  a2 = 90*Change;
-
-
-
-  //if (a2 != 0) {
-  //  a2 = 90*Change;
-  //}
-  //a2 = 90*Change;
-  //endif 
-
-
-
-  //cx, cy - coordinates of origin of the the system of fixed bar
+  degreeValue = PI / 180;
+  a1 = a1_change*degreeValue; 
+  a2 = a2_change*degreeValue;
+  
   cx = width / 2;
   cy = height / 4;
-
 
   buffer = createGraphics(width, height);
   buffer.background(255);
   buffer.translate(cx, cy);
 
-}
+  createSpan("What's your name? "); //label for entry1
+  // createInput([value], [type])
+  // type: "text" (default), "number",
+  // "date", "password", "email", etc.
+  entry1 = createInput();
+  //If text in the entry field changes, call
+  //the entryCallback function.
+  entry1.changed(entryCallback);
 
+}
 
 
 function draw() {
@@ -81,69 +86,6 @@ function draw() {
     Equation of Motion of the angular acceleration of the fixedBar
   */
 
-  // let num1 = -gravitationalAcceleration * (2 * massFixedObject + massAttachedObject) * math.sin(a1);
-  // let num2 = -massAttachedObject * gravitationalAcceleration * math.sin(a1 - 2 * a2);
-  // let num3 = -2 * math.sin(a1 - a2) * massAttachedObject;
-  // let num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar + angularVelocityFixedObject * angularVelocityFixedObject * fixedBar * math.cos(a1 - a2);
-  // let den = fixedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
-  // let angularAccelerationFixedBar = (num1 + num2 + num3 * num4) / den;
-
-  // /*
-  //   Equation of Motion of the angular acceleration of the attachedBar
-  // */
- 
-  // num1 = 2 * math.sin(a1 - a2); 
-  // num2 = (angularVelocityFixedObject * angularVelocityFixedObject * fixedBar * (massFixedObject + massAttachedObject));
-  // num3 = gravitationalAcceleration * (massFixedObject + massAttachedObject) * math.cos(a1);
-  // num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar * massAttachedObject * math.cos(a1 - a2);
-  // den = attachedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
-  // let angularAccelerationAttachedBar = (num1 * (num2 + num3 + num4)) / den;
-
-  document.getElementById("btn").addEventListener("click", PendulumButton);
-
-  var a3 = parseInt(document.getElementById("Theta1").value);
-  var a4 = parseInt(document.getElementById("Theta2").value);
-
-  function PendulumButton() {
-   
-  //alert("This function is being called")
-  // var a3 = parseInt(document.getElementById("Theta1").value);
-  
-  // var a4 = parseInt(document.getElementById("Theta2").value);
-
-  a1 = a3*Change;
-  a2 = a4*Change;
-
-  num1 = -gravitationalAcceleration * (2 * massFixedObject + massAttachedObject) * math.sin(a1);
-  num2 = -massAttachedObject * gravitationalAcceleration * math.sin(a1 - 2 * a2);
-  num3 = -2 * math.sin(a1 - a2) * massAttachedObject;
-  num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar + angularVelocityFixedObject * angularVelocityFixedObject * fixedBar * math.cos(a1 - a2);
-  den = fixedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
-  angularAccelerationFixedBar = (num1 + num2 + num3 * num4) / den;
-
-  num1 = 2 * math.sin(a1 - a2); 
-  num2 = (angularVelocityFixedObject * angularVelocityFixedObject * fixedBar * (massFixedObject + massAttachedObject));
-  num3 = gravitationalAcceleration * (massFixedObject + massAttachedObject) * math.cos(a1);
-  num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar * massAttachedObject * math.cos(a1 - a2);
-  den = attachedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
-  angularAccelerationAttachedBar = (num1 * (num2 + num3 + num4)) / den;
-
-
-  // a1 = a3*Change; 
-  // a2 = a4*Change;
-
-  }
-
-
-  if (a3 != null) {
-    a1 = a3*Change; 
-    a2 = a4*Change;
-  } else {
-    a1 = 90*Change
-    a2 = 90*Change;
-
-  }
-  
   let num1 = -gravitationalAcceleration * (2 * massFixedObject + massAttachedObject) * math.sin(a1);
   let num2 = -massAttachedObject * gravitationalAcceleration * math.sin(a1 - 2 * a2);
   let num3 = -2 * math.sin(a1 - a2) * massAttachedObject;
@@ -161,8 +103,6 @@ function draw() {
   num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar * massAttachedObject * math.cos(a1 - a2);
   den = attachedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
   let angularAccelerationAttachedBar = (num1 * (num2 + num3 + num4)) / den;
-
-  
 
   translate(cx, cy);
   stroke(0);
@@ -187,7 +127,7 @@ function draw() {
   a1 += angularVelocityFixedObject;
   a2 += angularVelocityAttachedObject;
 
-  angularVelocityFixedObject *= 0.9999; // this is the dampening
+  angularVelocityFixedObject *= 0.9999; // multiplication by 0.9999 is the dampening effect
   angularVelocityAttachedObject *= 0.9999;
 
   buffer.stroke(0);
@@ -200,5 +140,40 @@ function draw() {
   
   //console.log("Values of fixedBar", x1, y1);
   //console.log("Values of attachedBar", x2, y2);
+  //console.log(frameCount);
 }
 
+function eqqAngularAccelerationFixedBar(){
+  let num1 = -gravitationalAcceleration * (2 * massFixedObject + massAttachedObject) * math.sin(a1);
+  let num2 = -massAttachedObject * gravitationalAcceleration * math.sin(a1 - 2 * a2);
+  let num3 = -2 * math.sin(a1 - a2) * massAttachedObject;
+  let num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar + angularVelocityFixedObject * angularVelocityFixedObject * fixedBar * math.cos(a1 - a2);
+  let den = fixedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
+  angularAccelerationFixedBar = (num1 + num2 + num3 * num4) / den;
+  return angularAccelerationFixedBar;
+}
+
+function eqmAngularAccelerationAttachedBar(){
+  num1 = 2 * math.sin(a1 - a2); 
+  num2 = (angularVelocityFixedObject * angularVelocityFixedObject * fixedBar * (massFixedObject + massAttachedObject));
+  num3 = gravitationalAcceleration * (massFixedObject + massAttachedObject) * math.cos(a1);
+  num4 = angularVelocityAttachedObject * angularVelocityAttachedObject * attachedBar * massAttachedObject * math.cos(a1 - a2);
+  den = attachedBar * (2 * massFixedObject + massAttachedObject - massAttachedObject * math.cos(2 * a1 - 2 * a2));
+  angularAccelerationAttachedBar = (num1 * (num2 + num3 + num4)) / den;
+  return angularAccelerationAttachedBar;
+}
+
+function openIt(){
+  const url = "/index.html";
+  window.open(url,"something","width=800,height=800");
+  
+}
+
+//callback function for entry1
+function entryCallback() {
+  for (let i = 0; i < 25; i++) {
+    text(entry1.value(), random(width),
+          random(height));
+  }
+
+}
